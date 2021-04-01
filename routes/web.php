b78227dedfd9791562c4b912857b6417f065cfd3
Michael Tokar <michaeltokardeveloper@gmail.com>
2021-04-01 19:01:41 +0300

    Added middware for teacher.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Teachers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware(['teacher'])->group(function () {
+    Route::get('/profile', [TeacherController::class, 'index']);
+});
